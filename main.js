d276dfc96c08419fb7c9a942c50fb3131ca2c3fe
VincentSequerah <v1jnicholas@gmail.com>
2024-09-11 00:01:59 +0700

    update JS

---
+++ main.js
@@ -0,0 +1,11 @@
+const hexValues = [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F];
+
+const changeColour =document.getElementById(#changeColour);
+
+Function valueSelector ()
+A= math.floor(Math.random()*15);
+
+hexValues[A]
+
+
+console.log values

6f80038f2fabfbc75d48fda45492604c5e3138c5
VincentSequerah <v1jnicholas@gmail.com>
2024-09-11 13:12:10 +0700

    added copy button, changed css to adjust for screen size.

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
 const changeColour = document.getElementById("changeColour");
 const currentColour = document.getElementById('currentColour');
+let newColour= '#FFFFFF';
 
-changeColour.addEventListener('click', function () {
+changeColour.addEventListener('click', function () {    //change colour
     
-let newColour = '#';
+newColour = '#';
     for (let i=0; i<6; i++ ){
         const randomNumber = Math.floor(Math.random()*hexValues.length);
 
@@ -16,4 +17,8 @@ let newColour = '#';
 
 document.body.style.backgroundColor=newColour;
 currentColour.innerText=newColour;
+});
+
+copyColour.addEventListener('click', function(){      //copy colour
+    navigator.clipboard.writeText(newColour);
 });
